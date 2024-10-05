afeaefc35c805c7bca7dbf3118f8ae34bc88feeb
ZeathDev <zeathdev@outlook.com>
2024-10-05 21:38:03 +0800

    Bug fix in Velocity.java and new BetterJump implementation in onUpdate method

--- src/main/java/dev/hacksoar/modules/impl/combat/Velocity.java
+++ src/main/java/dev/hacksoar/modules/impl/combat/Velocity.java
@@ -4,6 +4,7 @@ import dev.hacksoar.api.events.EventTarget;
 import dev.hacksoar.api.events.impl.EventReceivePacket;
 import dev.hacksoar.api.events.impl.EventUpdate;
 import dev.hacksoar.api.tags.ModuleTag;
+import dev.hacksoar.api.value.impl.BoolValue;
 import dev.hacksoar.api.value.impl.ListValue;
 import dev.hacksoar.modules.Module;
 import dev.hacksoar.modules.ModuleCategory;
@@ -16,9 +17,18 @@ import net.minecraft.potion.Potion;
 @ModuleTag
 public class Velocity extends Module {
     private final ListValue mode = new ListValue("Mode", new String[]{"Cancel", "Jump Reset", "Watchdog"}, "Cancel");
+    private final BoolValue betterJump = new BoolValue("Better Jump Reset", true, () -> mode.isMode("Jump Reset"));
+
+    private boolean jump = false;
 
     public Velocity() {
         super("Velocity", "No kb", ModuleCategory.Combat);
+    }
+
+    @Override
+    public void onDisable() {
+        jump = false;
+        super.onDisable();
     }
 
     @EventTarget
@@ -46,6 +56,17 @@ public class Velocity extends Module {
     @EventTarget
     public void onUpdate(EventUpdate event) {
         if (mode.isMode("Jump Reset")) {
+            if (!betterJump.get()) {
+                if (mc.thePlayer.hurtTime > 0 && mc.thePlayer.onGround) {
+                    mc.gameSettings.keyBindJump.setPressed(true);
+                    jump = true;
+                } else if (jump) {
+                    mc.gameSettings.keyBindJump.setPressed(false);
+                    jump = false;
+                }
+                return;
+            }
+
             if (mc.thePlayer.hurtTime >= 8) {
                 mc.gameSettings.keyBindJump.setPressed(true);
             }
